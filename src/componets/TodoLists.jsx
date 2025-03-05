52aabef74180b6b1ddbfcaa8faf071078e7a8b04
sungmin <ehdtls54@naver.com>
2025-03-05 16:12:48 +0900

    category 추가 , 스타일 적용

--- src/componets/TodoLists.jsx
+++ src/componets/TodoLists.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import classNames from 'classnames';
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "./TodoLists.css"
 
-function TodoLists(){
+function TodoLists({todos}){
+  const {categoryColor} = todos
+  const titleClass = classNames("category", categoryColor);
+  function returnDday(todo) {
+    return Math.round(new Date(todo.end).getTime()- new Date(todo.start).getTime())/(1000*60*60*24)
+  }
     return(
-      <ul>
-        <li>할일 목록이 여기에 표기됩니다.</li>
+      <>
+      <ul className="items">
+        {
+        todos.map(todo => <li key={todo.id} className="item">
+          <div className="category">{todo.category}</div>
+          <div className="main">
+          <h2 className='text-primary'>{todo.title}</h2>
+          <p className='text-secondary'>{todo.desc}</p>
+          <p><i className="bi bi-people gap-1"></i> {todo.participant}</p>
+          </div>
+          <div className="extra"><span><i className="bi bi-calendar-check"></i> {returnDday(todo)}</span></div>
+        </li>)
+        }
       </ul>
+      </>
     )
   }
 
