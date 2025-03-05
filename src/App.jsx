09dc6ada1a6e37fd6503bbff16d5adb8ea5125a1
sungmin <ehdtls54@naver.com>
2025-03-05 16:12:24 +0900

    category추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,63 @@ import TodoLists from './componets/TodoLists'
 import "./App.css"
 
 
+
 function App() {
-  const [idx, setIdx] = useState(0);
-
-
+  const [id, setId] = useState(0);
+  const [todos,setTodos] = useState([]);
   const [formData, setFormData] = useState({
     title:'',
     desc:'',
+    category:'',
+    participant:"",
+    color:"",
     start: new Date().toISOString().split("T")[0],
     end:''
   })
 
   function onSubmit(e){
     e.preventDefault();
-    // setTodos(prevTodos => [...prevTodos , {
-    //   id: idx,
-    //   subject : title,
-    //   desc : description,
-    //   start : startDate,
-    //   end : endDate
-    // }])
-  }
+    setTodos(prevTodo => ([
+      ...prevTodo, {
+      ...formData,
+      id:id
+    }]))
+    setId(prevId => prevId + 1);
 
-function onChange(e) {
-  const {name,value} = e.target
-  console.log(name,value);
-}
+    setFormData({
+    title:'',
+    desc:'',
+    category:'',
+    participant:"",
+    color:"",
+    start: new Date().toISOString().split("T")[0],
+    end:''
+    })
+  }
+  
+  function onChange(e) {
+    const {name,value} = e.target
+    // console.log(name,value);
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }))
+
+  }
+  function onClick(e){
+    console.dir(e.target.dataset)
+    setFormData(prevData => ({
+      ...prevData,
+      ["category"]: e.target.innerText,
+      ["color"]: e.target.dataset.color
+    }))
+  }
 
   return (
     <>
     <TodoHeader />
-    <TodoInput onSubmit={onSubmit} formData={formData} onChange={onChange} />
-    <TodoLists />
+    <TodoInput onSubmit={onSubmit} onClick={onClick} formData={formData} onChange={onChange}/>
+    <TodoLists todos={todos} />
     </>
   )
 }
--- src/componets/TodoInput.jsx
+++ src/componets/TodoInput.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 
-function TodoInput({formData,onChange,onSubmit}){
+function TodoInput({formData,onChange,onSubmit,onClick}){
     return (
     <form onSubmit={onSubmit}>
         <fieldset>
             <legend>Todo App</legend>
-        <div className="d-flex flex-column gap-1 container">
-            <input type="text" placeholder="title here" name="title" value={formData.title} onChange={onChange}/>
-            <textarea rows="5" name="desc" clos="20" placeholder="add a description..." value={formData.desc} onChange={onChange} ></textarea>
+        <div className="d-flex flex-column gap-1 container justify-content-center">
+            <div className="d-flex justify-content-between">
+                <button type="button" onClick={onClick} className="btn btn-color" data-color="red">free time</button>
+                <button type="button" onClick={onClick} className="btn" data-color="blue">travel</button>
+                <button type="button" onClick={onClick} className="btn" data-color="green">meting</button>
+                <button type="button" onClick={onClick} className="btn" data-color="black">atc</button>
+            </div>
+            <input type="text" placeholder="제목" name="title" value={formData.title} onChange={onChange}/>
+            <textarea rows="5" name="desc" clos="20" placeholder="내용" value={formData.desc} onChange={onChange} ></textarea>
             <div className="d-flex gap-1 align-items-center justify-content-between">
                 <label htmlFor="start-date">시작일 </label>
                 <input type="date" id="start" name="start" value={formData.start}  onChange={onChange} className="m-0" />
                 <label htmlFor="end-date">종료일 </label>
                 <input type="date" id="end" name="end" value={formData.end} onChange={onChange} className="m-0"/>
             </div>
-                <input type="submit" value="등록"/>
+                <div className='d-flex gap-1 align-items-center justify-content-between'>
+                    <label className='d-flex gap-1'>인 원</label>
+                    <input min="0" max="20" type="number" id='participant' name='participant' onChange={onChange} />
+                </div>
+                <input type="submit" value="등록" className='btn-active'/>
         </div>
         </fieldset>
     </form>
