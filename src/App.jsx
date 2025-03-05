d027088f4b6bcf745e41103d0659911e97f3b92b
sungmin <ehdtls54@naver.com>
2025-03-05 10:32:11 +0900

    이벤트 핸들러 하나로 통합 및 오류수정완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,35 @@ import "./App.css"
 
 function App() {
   const [idx, setIdx] = useState(0);
-  const [title, setTitle] = useState("");
-  const [description,setDescription] = useState("");
-  const [startDate,setStartDate] = useState(new Date());
-  const [endDate,setEndDate] = useState("");
+
+
+  const [formData, setFormData] = useState({
+    title:'',
+    desc:'',
+    start: new Date().toISOString().split("T")[0],
+    end:''
+  })
 
   function onSubmit(e){
     e.preventDefault();
-    setTodos(prevTodos => [...prevTodos , {
-      id: idx,
-      subject : title,
-      desc : description,
-      start : startDate,
-      end : endDate
-    }])
+    // setTodos(prevTodos => [...prevTodos , {
+    //   id: idx,
+    //   subject : title,
+    //   desc : description,
+    //   start : startDate,
+    //   end : endDate
+    // }])
   }
-  function onTitleChange(e) {
-    setTitle(e.target.velue)
-  }
-  function onDescChange(e) {
-    setDescription(e.target.velue)
-  }
-  function onStartDateChange(e) {
-    setStartDate(new Date())
-  }
-  function onEndDateChange(e) {
-    setEndDate(e.target.velue)
-  }
+
+function onChange(e) {
+  const {name,value} = e.target
+  console.log(name,value);
+}
 
   return (
     <>
     <TodoHeader />
-    <TodoInput startDate={startDate} onTitleChange={onTitleChange} onDescChange={onDescChange} onStartDateChange={onStartDateChange} onEndDateChange={onEndDateChange} onSubmit={onSubmit} title={title} description={description} />
+    <TodoInput onSubmit={onSubmit} formData={formData} onChange={onChange} />
     <TodoLists />
     </>
   )
--- src/componets/TodoInput.jsx
+++ src/componets/TodoInput.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-function TodoInput({startDate,endDate,description,title,onDescChange,onTitleChange,onStartDateChange,onEndDateChange,onSubmit,onChange}){
+function TodoInput({formData,onChange,onSubmit}){
     return (
     <form onSubmit={onSubmit}>
         <fieldset>
             <legend>Todo App</legend>
         <div className="d-flex flex-column gap-1 container">
-            <input type="text" placeholder='todo title' value={title} onChange={(e)=> onTitleChange(e.target.value)} />
-            <textarea rows="5" clos="20" placeholder="add a description..." value={description} onChange={(e)=> onDescChange(e.target.value)} ></textarea>
+            <input type="text" placeholder="title here" name="title" value={formData.title} onChange={onChange}/>
+            <textarea rows="5" name="desc" clos="20" placeholder="add a description..." value={formData.desc} onChange={onChange} ></textarea>
             <div className="d-flex gap-1 align-items-center justify-content-between">
-                <label htmlFor="start-date">시작일 </label><input type="date" id='start-date' value={startDate}  onChange={(e)=> onStartDateChange(e.target.value)} className="m-0" />
-                <label htmlFor="end-date">종료일 </label><input type="date" id='end-end' value={endDate} onChange={(e)=> onEndDateChange(e.target.value)} className="m-0"/>
+                <label htmlFor="start-date">시작일 </label>
+                <input type="date" id="start" name="start" value={formData.start}  onChange={onChange} className="m-0" />
+                <label htmlFor="end-date">종료일 </label>
+                <input type="date" id="end" name="end" value={formData.end} onChange={onChange} className="m-0"/>
             </div>
                 <input type="submit" value="등록"/>
         </div>
